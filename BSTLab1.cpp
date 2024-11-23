e6d1476e522d655c9fc1a674fe6292fde922bd9b
camilarrez <161494674+camilarrez@users.noreply.github.com>
2024-11-23 20:06:26 +0000

    initial code for create tree

--- BSTLab1.cpp
+++ BSTLab1.cpp
@@ -8,11 +8,18 @@ class Node {
 		// the Node should contain
 		// 	- an integer "key" that holds the value of the Node
 		// 	- a pointer to each child possible child node ("right" and "left")
-
+	int key;
+	Node *right;
+	Node *left;
 
 		// Write a constructor for the Node
 		// input: only an integer value for "key", no pointer arguments
 		// 		right and left pointers should be null
+	Node(int value = 0){
+		kay = value;
+		right = nullptr;
+		left = nullptr;
+	}
 
 };
 
@@ -24,20 +31,26 @@ Node* createTree() {
 	Node *root = nullptr;
 
 	// level 1 (children of root)
-
+root->left = new Node(0);
+root->right = new Node(21)
 	// level 2 (children of 0)
-
+root->left->left = new Node(-2);
+root->left->right = new Node(7);
 	// level 2 (children of 21)
+	root->right->left = new Node(15);
+	root->right->right = new Node(99);
 
 	// level 3 (children of -2)
-
+root->left->left->right = new Node(-1);
 	// level 3 (children of 7)
-
+root->left->right->left = new Node(6);
+root->left->right->right = new Node(12);
 	// level 3 (children of 15)
-
+root->right->left->right = new Node(18);
 	// level 3 (children of 99)
 
 	// level 4 (children of 18)
+	root->right->left->right->right = new Node(19);
 
 	return root;
 }
@@ -48,10 +61,17 @@ Node* createTree() {
 // 	returns: true or false depending on if the value is found
 // You should write this function recursively! What is the base case? What is the general case?
 bool searchTree(int target, Node* root) {
-	// Base cases
-	return false;
+	// Base casee
+	if(root == nullptr){
+		return false;
+	}
 
 	// General case
+	if(root->data = target){
+		return true;
+	}else(){
+		
+	}
 	return false;
 	
 }
